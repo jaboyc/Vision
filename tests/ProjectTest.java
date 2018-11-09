59a974d3bcb5290af9a656dba494a80a35b6a2dd
Jake Boychenko <jakeboychenko@yahoo.com>
2018-11-09 12:00:31 -0500

    Testing Saving and Opening a Project

--- tests/ProjectTest.java
+++ tests/ProjectTest.java
@@ -1,4 +1,5 @@
 import com.jlogical.vision.project.Project;
+import com.jlogical.vision.project.VisionFile;
 import org.junit.*;
 
 import java.io.File;
@@ -21,6 +22,33 @@ public class ProjectTest {
     public void testValidProject() throws Exception{
         assertNotNull(Project.fromFile("res/test.vproj"));
         assertNotNull(Project.fromFile(new File("res/test.vproj")));
+    }
+
+    @Test
+    public void testOpenProject() throws Exception{
+        Project project = Project.fromFile("res/test.vproj");
+        assertEquals(project.getName(), "test1");
+        assertEquals(project.getFiles().size(), 1);
+        assertEquals(project.getFiles().get(0).getName(), "main");
+        assertEquals(project.getFiles().get(0).getCode(), "when started\n\tprint [Hello World]\nend");
+    }
+
+    @Test
+    public void testSaveAndOpenProject() throws Exception{
+
+        File file = new File("res/mtest.vproj");
+
+        Project project = Project.blank("testOpenSave");
+        project.getFiles().add(new VisionFile("name", "code"));
+        project.save(file);
+
+        Project openProject = Project.fromFile(file);
+        assertEquals(openProject.getName(), "testOpenSave");
+        assertEquals(openProject.getFiles().size(), 1);
+        assertEquals(openProject.getFiles().get(0).getName(), "name");
+        assertEquals(openProject.getFiles().get(0).getCode(), "code");
+
+        file.delete();
     }
 
 }
