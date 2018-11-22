19ee09c96daa54920d79c8c89e1cb306165c2e8d
Jake <jakeboychenko@yahoo.com>
2018-11-22 02:10:43 -0500

    Added basic operations. Util functions for Parameters.

--- src/com/jlogical/vision/api/runnables/Parameters.java
+++ src/com/jlogical/vision/api/runnables/Parameters.java
@@ -28,32 +28,42 @@ public abstract class Parameters {
      * The range that this Parameter is covering. Used for exception throwing.
      */
     CodeRange range;
-    //TODO Use Range to throw exceptions!
 
     /**
      * Creates a Parameters with a given List of Values.
      */
-    public Parameters(ArrayList<Value> values, Hat hat, CodeRange range){
+    public Parameters(ArrayList<Value> values, Hat hat, CodeRange range) {
         this.values = values != null ? values : new ArrayList<Value>();
         this.hatHolder = hat;
         this.range = range;
     }
 
     /**
+     * Throws a VisionException with its range.
+     *
+     * @param message the error message.
+     * @throws VisionException always.
+     */
+    public void err(String message) throws VisionException {
+        throw new VisionException(message, range);
+    }
+
+    /**
      * Returns the value of the Value at the given index in the values List.
+     *
      * @param index the index of which Value to get the value from.
      * @return the value of the Value.
      * @throws VisionException if the index is negative or too big.
      */
-    public Object get(int index) throws VisionException{
-        if(index < 0){
+    public Object get(int index) throws VisionException {
+        if (index < 0) {
             throw new VisionException("Index cannot be null!", range);
         }
-        if(index >= values.size()){
+        if (index >= values.size()) {
             throw new VisionException("Index cannot be out of range of values!", range);
         }
         Value value = values.get(index);
-        if(value == null){
+        if (value == null) {
             throw new VisionException("A Value cannot be null!", range);
         }
         return value.getValue();
@@ -61,82 +71,126 @@ public abstract class Parameters {
 
     /**
      * Returns the String value of the Value at the given index.
+     *
      * @param index the index of the Value.
      * @return the String representation of the value.
      * @throws VisionException if the index is negative or too big.
      */
-    public String str(int index) throws VisionException{
-        return get(index).toString();
+    public String str(int index) throws VisionException {
+        return toString(get(index));
+    }
+
+    /**
+     * Returns the given value as a String. Used as a util command.
+     *
+     * @param value the value to convert.
+     * @return the String value of the given input.
+     */
+    public String toString(Object value) {
+        return value.toString();
     }
 
     /**
      * Returns the int value of the Value at the given index.
+     *
      * @param index the index of the Value.
      * @return the int representation of the Value.
      * @throws VisionException if the index is negative, too big, or cannot be converted to an integer.
      */
     public int numInt(int index) throws VisionException {
-        Object val = get(index);
-        if(val instanceof Integer){
+        return toInt(get(index));
+    }
+
+    /**
+     * Returns the given value as a int. Used as a util command.
+     *
+     * @param val the value to convert.
+     * @return the int value of the given input.
+     * @throws VisionException if the value cannot be converted to an int.
+     */
+    public int toInt(Object val) throws VisionException {
+        if (val instanceof Integer) {
             return (int) val;
-        }else if (val instanceof Double){
+        } else if (val instanceof Double) {
             return (int) val;
-        }else if (val instanceof Boolean){
+        } else if (val instanceof Boolean) {
             return ((boolean) val) ? 1 : 0;
         }
         try {
             return (int) Double.parseDouble(val.toString());
         } catch (NumberFormatException e) {
-            throw new VisionException("Cannot convert '"+val+"' to an integer!", range);
+            throw new VisionException("Cannot convert '" + val + "' to an integer!", range);
         }
     }
 
     /**
      * Returns the double value of the Value at the given index.
+     *
      * @param index the index of the Value.
      * @return the double representation of the Value.
      * @throws VisionException if the index is negative, too big, or cannot be converted to a number.
      */
-    public double num(int index) throws VisionException{
-        Object val = get(index);
-        if(val instanceof Integer){
+    public double num(int index) throws VisionException {
+        return toNum(get(index));
+    }
+
+    /**
+     * Returns the given value as a double. Used as a util command.
+     *
+     * @param val the value to convert.
+     * @return the double value of the given input.
+     * @throws VisionException if the value cannot be converted to an double.
+     */
+    public double toNum(Object val) throws VisionException {
+        if (val instanceof Integer) {
             return (double) val;
-        }else if (val instanceof Double){
+        } else if (val instanceof Double) {
             return (double) val;
-        }else if (val instanceof Boolean){
-            return ((boolean) val) ? 1: 0;
+        } else if (val instanceof Boolean) {
+            return ((boolean) val) ? 1 : 0;
         }
         try {
             return Double.parseDouble(val.toString());
         } catch (NumberFormatException e) {
-            throw new VisionException("Cannot convert '"+val+"' to a number!", range);
+            throw new VisionException("Cannot convert '" + val + "' to a number!", range);
         }
     }
 
     /**
      * Returns the boolean value of the Value at the given index.
+     *
      * @param index the index of the Value.
      * @return the boolean representation of the Value.
      * @throws VisionException if the index is negative, too big, or cannot be converted to a boolean.
      */
-    public boolean bool(int index) throws VisionException{
-        Object val = get(index);
-        if(val instanceof Boolean){
+    public boolean bool(int index) throws VisionException {
+        return toBoolean(get(index));
+    }
+
+    /**
+     * Returns the given value as a boolean. Used as a util command.
+     *
+     * @param val the value to convert.
+     * @return the boolean value of the given input.
+     * @throws VisionException if the value cannot be converted to an boolean.
+     */
+    public boolean toBoolean(Object val) throws VisionException {
+        if (val instanceof Boolean) {
             return (boolean) val;
-        } else if(val instanceof Integer || val instanceof Double){
+        } else if (val instanceof Integer || val instanceof Double) {
             return ((int) val) != 0;
-        }else if (val instanceof String){
+        } else if (val instanceof String) {
             String sVal = (String) val;
-            if(sVal.equalsIgnoreCase("true")){
+            if (sVal.equalsIgnoreCase("true")) {
                 return true;
-            }else if(sVal.equalsIgnoreCase(("false"))){
+            } else if (sVal.equalsIgnoreCase(("false"))) {
                 return false;
             }
         }
-        throw new VisionException("Cannot convert '"+val+"' to an boolean!", range);
+        throw new VisionException("Cannot convert '" + val + "' to a boolean!", range);
     }
 
-    public Script getScript(){
+    public Script getScript() {
         return hatHolder.getScript();
     }
 
@@ -146,5 +200,9 @@ public abstract class Parameters {
 
     public Hat getHatHolder() {
         return hatHolder;
+    }
+
+    public CodeRange getRange() {
+        return range;
     }
 }
--- src/com/jlogical/vision/api/system/CoreAPI.java
+++ src/com/jlogical/vision/api/system/CoreAPI.java
@@ -1,6 +1,7 @@
 package com.jlogical.vision.api.system;
 
 import com.jlogical.vision.api.API;
+import com.jlogical.vision.compiler.exceptions.VisionException;
 import com.jlogical.vision.compiler.script.Variable;
 import com.jlogical.vision.project.Project;
 
@@ -15,6 +16,16 @@ public class CoreAPI extends API {
     public CoreAPI(Project project) {
         super(project);
 
+        coreCommands();
+        variableCommands();
+        mathLogicCommands();
+    }
+
+    /**
+     * Adds the commands and hats related to the core functionality of Vision.
+     */
+    private void coreCommands() {
+
         //Hats
         addHat("when started");
 
@@ -24,33 +35,77 @@ public class CoreAPI extends API {
             System.out.println(output);
             p.getScript().appendOutputLog(output);
         });
-        addCommand("set [] to []", p->{
+
+    }
+
+    /**
+     * Adds the commands and reporters related to Variables.
+     */
+    private void variableCommands() {
+
+        //Commands
+        addCommand("set [] to []", p -> {
             String name = p.str(0);
-            Variable variable = Variable.findVariable(name,  p.getHatHolder(), p.getScript());
-            if(variable == null){
+            Variable variable = Variable.findVariable(name, p.getHatHolder(), p.getScript());
+            if (variable == null) {
                 p.getHatHolder().getVariables().add(new Variable(name, p.get(1)));
-            }else{
+            } else {
                 variable.setValue(p.get(1));
             }
         });
-        addCommand("set global [] to []", p->{
+        addCommand("change [] by []", p -> {
+            String name = p.str(0);
+            Variable variable = Variable.findVariable(name, p.getHatHolder(), p.getScript());
+            if (variable == null) {
+                p.err("Cannot find variable named '" + name + "'");
+            }
+            variable.setValue(p.toNum(variable.getValue()) + p.num(1));
+        });
+        addCommand("set global [] to []", p -> {
             String name = p.str(0);
             Variable variable = Variable.findGlobalVariable(name, p.getScript());
-            if(variable == null){
+            if (variable == null) {
                 p.getScript().getVariables().add(new Variable(name, p.get(1)));
-            }else{
+            } else {
                 variable.setValue(p.get(1));
             }
+        });
+        addCommand("change global [] by []", p -> {
+            String name = p.str(0);
+            Variable variable = Variable.findGlobalVariable(name, p.getScript());
+            if (variable == null) {
+                p.err("Cannot find global variable named '" + name + "'");
+            }
+            variable.setValue(p.toNum(variable.getValue()) + p.num(1));
         });
 
         //Reporters
         addReporter("value of []", p -> {
-           String name = p.str(0);
-           Variable variable = Variable.findVariable(name, p.getHatHolder(), p.getScript());
-           if(variable == null) {
-               throw new NullPointerException("Cannot find variable '"+ name+"'");
-           }
-           return variable.getValue();
+            String name = p.str(0);
+            Variable variable = Variable.findVariable(name, p.getHatHolder(), p.getScript());
+            if (variable == null) {
+                p.err("Cannot find variable '" + name + "'");
+            }
+            return variable.getValue();
         });
+        addReporter("value of global []", p -> {
+            String name = p.str(0);
+            Variable variable = Variable.findGlobalVariable(name, p.getScript());
+            if (variable == null) {
+                p.err("Cannot find global variable '" + name + "'");
+            }
+            return variable.getValue();
+        });
+    }
+
+    /**
+     * Adds the reporters related to math and logic.
+     */
+    private void mathLogicCommands() {
+        addReporter("[] + []", p -> p.num(0) + p.num(1));
+        addReporter("[] - []", p -> p.num(0) - p.num(1));
+        addReporter("[] * []", p -> p.num(0) * p.num(1));
+        addReporter("[] / []", p->p.num(0) / p.num(1));
+        addReporter("[] ^ []", p->Math.pow(p.num(0), p.num(1)));
     }
 }
